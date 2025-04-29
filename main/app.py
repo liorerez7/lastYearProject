7761986876aec13ed98fb156e7dea9ed8511e3b4
Lior <liorer@mta.ac.il>
2025-04-29 11:08:35 +0300

    now working with port

--- main/app.py
+++ main/app.py
@@ -1,11 +1,7 @@
-import traceback
-
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import JSONResponse
 
 from main.api.migration_controller import migrationRouter
-from main.api.test_controller import testRouter
+from main.api1.test_controller import testRouter
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import traceback
@@ -21,7 +17,6 @@ app.add_middleware(
 app.include_router(migrationRouter, prefix="/migration")
 app.include_router(testRouter, prefix="/test")
 
-
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
     print("🔥🔥🔥 GLOBAL CATCH EXCEPTION 🔥🔥🔥")
@@ -31,3 +26,12 @@ async def global_exception_handler(request: Request, exc: Exception):
         status_code=500,
         content={"detail": f"Internal Server Error: {str(exc)}"},
     )
+
+@app.on_event("startup")
+def list_routes():
+    print("=== ROUTES LOADED ===")
+    for route in app.router.routes:
+        print(f"{route.methods} {route.path}")
+
+
+
