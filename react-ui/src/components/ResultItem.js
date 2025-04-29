332adf8c04a39ac803e23000f04c84be6517c524
Lior <liorer@mta.ac.il>
2025-04-29 16:33:01 +0300

    working with also adding to the data base

--- react-ui/src/components/ResultItem.js
+++ react-ui/src/components/ResultItem.js
@@ -7,6 +7,8 @@ const ResultItem = ({ result }) => {
       <p><b>Query Type:</b> {result.query_type}</p>
       <p><b>Repeat:</b> {result.repeat}</p>
       <p><b>Selector:</b> {result.selector}</p>
+      <p><b>Durations:</b> {result.durations?.join(", ") || "N/A"}</p>
+      <p><b>Std Dev:</b> {result.stddev || "N/A"}</p>
     </div>
   );
 };
