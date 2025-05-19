87c1058652e41fd4b1cfe5c475ded0824ce79d1b
Lior <liorer@mta.ac.il>
2025-05-19 13:16:42 +0300

    fixed indexes in correct syntax

--- main/core/scripts/create_indexes.py
+++ main/core/scripts/create_indexes.py
@@ -1,7 +1,6 @@
 from sqlalchemy import text
 from main.core.schema_analysis.connection.db_connector import DBConnector
 
-# רשימת האינדקסים הרצויים לפי DB
 MYSQL_INDEX_QUERIES = [
     ("employees", "idx_emp_no_employees", "CREATE INDEX idx_emp_no_employees ON employees(emp_no);"),
     ("salaries", "idx_emp_no_salaries", "CREATE INDEX idx_emp_no_salaries ON salaries(emp_no);"),
@@ -11,17 +10,13 @@ MYSQL_INDEX_QUERIES = [
     ("employees", "idx_birth_date_employees", "CREATE INDEX idx_birth_date_employees ON employees(birth_date);"),
     ("employees", "idx_emp_birth_employees", "CREATE INDEX idx_emp_birth_employees ON employees(emp_no, birth_date);"),
     ("salaries", "idx_emp_to_date", "CREATE INDEX idx_emp_to_date ON salaries(emp_no, to_date);"),
-    ("salaries", "idx_emp_salary", "CREATE INDEX idx_emp_salary ON salaries(emp_no, salary);")
-
+    ("salaries", "idx_emp_salary", "CREATE INDEX idx_emp_salary ON salaries(emp_no, salary);"),
     ("dept_emp", "idx_dept_emp_combo", "CREATE INDEX idx_dept_emp_combo ON dept_emp(emp_no, dept_no, from_date);"),
     ("salaries", "idx_salaries_emp_to_date", "CREATE INDEX idx_salaries_emp_to_date ON salaries(emp_no, to_date);"),
-    ("titles", "idx_titles_emp_no", "CREATE INDEX idx_titles_emp_no ON titles(emp_no);")
+    ("titles", "idx_titles_emp_no", "CREATE INDEX idx_titles_emp_no ON titles(emp_no);"),
     ("salaries", "idx_salary_to_date", "CREATE INDEX idx_salary_to_date ON salaries(salary, to_date);"),
-    ("salaries", "idx_window_salary_to_date_emp", "CREATE INDEX idx_window_salary_to_date_emp ON salaries(salary, to_date DESC, emp_no);")
+    ("salaries", "idx_window_salary_to_date_emp", "CREATE INDEX idx_window_salary_to_date_emp ON salaries(salary, to_date DESC, emp_no);"),
     ("salaries", "idx_window_emp_to_date_salary", "CREATE INDEX idx_window_emp_to_date_salary ON salaries(emp_no, to_date DESC, salary);")
-
-
-
 ]
 
 PG_INDEX_QUERIES = [
@@ -32,23 +27,13 @@ PG_INDEX_QUERIES = [
     ("departments", "idx_dept_no_departments", 'CREATE INDEX idx_dept_no_departments ON "extendedEmp".departments(dept_no);'),
     ("employees", "idx_birth_date_employees", 'CREATE INDEX idx_birth_date_employees ON "extendedEmp".employees(birth_date);'),
     ("employees", "idx_emp_birth_employees", 'CREATE INDEX idx_emp_birth_employees ON "extendedEmp".employees(emp_no, birth_date);'),
-    ("salaries", "idx_emp_to_date",
-     'CREATE INDEX IF NOT EXISTS idx_emp_to_date '
-     'ON "extendedEmp".salaries(emp_no, to_date);'),
-    ("salaries", "idx_emp_salary",
-     'CREATE INDEX IF NOT EXISTS idx_emp_salary '
-     'ON "extendedEmp".salaries(emp_no, salary);')
-
-    ("dept_emp", "idx_dept_emp_combo",
-     'CREATE INDEX idx_dept_emp_combo ON "extendedEmp".dept_emp(emp_no, dept_no, from_date);'),
-    ("salaries", "idx_salaries_emp_to_date",
-     'CREATE INDEX idx_salaries_emp_to_date ON "extendedEmp".salaries(emp_no, to_date);'),
-    ("titles", "idx_titles_emp_no", 'CREATE INDEX idx_titles_emp_no ON "extendedEmp".titles(emp_no);')
-
+    ("salaries", "idx_emp_to_date", 'CREATE INDEX IF NOT EXISTS idx_emp_to_date ON "extendedEmp".salaries(emp_no, to_date);'),
+    ("salaries", "idx_emp_salary", 'CREATE INDEX IF NOT EXISTS idx_emp_salary ON "extendedEmp".salaries(emp_no, salary);'),
+    ("dept_emp", "idx_dept_emp_combo", 'CREATE INDEX idx_dept_emp_combo ON "extendedEmp".dept_emp(emp_no, dept_no, from_date);'),
+    ("salaries", "idx_salaries_emp_to_date", 'CREATE INDEX idx_salaries_emp_to_date ON "extendedEmp".salaries(emp_no, to_date);'),
+    ("titles", "idx_titles_emp_no", 'CREATE INDEX idx_titles_emp_no ON "extendedEmp".titles(emp_no);'),
     ("salaries", "idx_salary_to_date", 'CREATE INDEX idx_salary_to_date ON "extendedEmp".salaries(salary, to_date);'),
     ("salaries", "idx_window_salary_to_date_emp", 'CREATE INDEX idx_window_salary_to_date_emp ON "extendedEmp".salaries(salary, to_date DESC, emp_no);')
-
-
 ]
 
 
