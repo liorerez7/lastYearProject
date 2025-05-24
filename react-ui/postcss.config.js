34de6fa588a5d05132632a6a45937780b995d12f
Lior <liorer@mta.ac.il>
2025-05-24 14:19:16 +0300

    working

---
+++ react-ui/postcss.config.js
@@ -0,0 +1,13 @@
+//module.exports = {
+//  plugins: {
+//    '@tailwindcss/postcss': {},   // ← הפלאג-אין החדש שחיפשנו
+//    autoprefixer: {},
+//  },
+//};
+// react-ui/postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},   // ← ברירת-המחדל של Tailwind 3
+    autoprefixer: {},
+  },
+};
